3c57af374d0c1907b93ec1591d4a2c6dd8b3d2b5
Shawn M. Jones <jones.shawn.m@gmail.com>
2021-05-19 14:54:20 -0600

    removed pinned numpy from sumgram

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         "Operating System :: OS Independent"
     ],
     install_requires=[
-        'numpy==1.17.0',
+        'numpy',
         'requests==2.22.0',
         'sklearn==0.0'
     ],
